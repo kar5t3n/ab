0cfa155f447be0902363cd391d2b5fbaaed47ed3
kar5t3n <kar5t3n@riseup.net>
2014-04-09 18:56:32 +0000

    Update host.sh

--- host.sh
+++ host.sh
@@ -5,3 +5,4 @@
 echo "NOTE: You need make a secure record of the following details for future reference. DO IT NOW!"
 echo "Hostname: $(cat /var/lib/tor/hidden_service/hostname)"
 echo "Admin Account: root@$(cat /var/lib/tor/hidden_service/hostname)"
+echo "when you're ready, restart the system by running 'reboot'"
