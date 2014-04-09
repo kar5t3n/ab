2d2a045d4068c7966d5a66e6f108b92be2049b8b
kar5t3n <kar5t3n@riseup.net>
2014-04-09 17:10:24 +0000

    Update host.sh

--- host.sh
+++ host.sh
@@ -2,6 +2,6 @@
 #
 # Run this script to view your Tor hidden service details
 
-echo "NOTE: You need make a secure record the following details for future reference. Do it now!"
+echo "NOTE: You need make a secure record of the following details for future reference. DO IT NOW!"
 echo "Hostname: $(cat /var/lib/tor/hidden_service/hostname)"
 echo "Admin Account: root@$(cat /var/lib/tor/hidden_service/hostname)"
