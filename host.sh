d512db7318ba3fa56aa88da2f4d95a5e58b1c049
kar5t3n <kar5t3n@riseup.net>
2014-04-09 20:04:53 +0000

    Delete host.sh

--- host.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Run this script to view your Tor hidden service details
-
-echo "PLEASE NOTE: You need make a secure record of the following details for future reference. DO IT NOW!"
-echo "Hostname: $(cat /var/lib/tor/hidden_service/hostname)"
-echo "Admin Account: root@$(cat /var/lib/tor/hidden_service/hostname)"
-echo "when you're ready, restart the system by running 'reboot'"
