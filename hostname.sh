260bf71f545220de9ec992cd13a3e4d8c7d97b3b
kar5t3n <kar5t3n@riseup.net>
2014-04-09 12:53:48 +0000

    Update hostname.sh

--- hostname.sh
+++ hostname.sh
@@ -2,6 +2,8 @@
 #
 # Run this script to view your Tor hidden service hostname
 
-cat /var/lib/tor/hidden_service/hostname
-
-echo "The admin account on the XPMM server is root@$(cat /var/lib/tor/hidden_service/hostname)"
+echo "The following information is required to access the Tor hidden service"
+echo "The Tor hidden service hostname is $(cat /var/lib/tor/hidden_service/hostname)"
+echo "The admin account on the XMPP server is root@$(cat /var/lib/tor/hidden_service/hostname)"
+echo "User account on the XMPP server will be username@$(cat /var/lib/tor/hidden_service/hostname)"
+echo "To access the server from Tails, run: ssh root@$(cat /var/lib/tor/hidden_service/hostname)"
