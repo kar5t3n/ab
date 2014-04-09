718530584bbbe484253e154236d27904d5957f66
kar5t3n <kar5t3n@riseup.net>
2014-04-09 13:12:30 +0000

    Update hostname.sh

--- hostname.sh
+++ hostname.sh
@@ -2,6 +2,6 @@
 #
 # Run this script to view your Tor hidden service details
 
-echo "Tor hidden service"
+echo "Tor Hidden Service"
 echo "Hostname: $(cat /var/lib/tor/hidden_service/hostname)"
-echo "Admin account: root@$(cat /var/lib/tor/hidden_service/hostname)"
+echo "Admin Account: root@$(cat /var/lib/tor/hidden_service/hostname)"
